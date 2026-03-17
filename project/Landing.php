ff0038d838031a7ea6708629729b24250be3cf48
Clemens Altrichter <c.altrichter@students.htl-leonding.ac.at>
2026-03-17 15:10:09 +0100

    fonts imported, basic project structure created, fonts imported, variables for colors written

---
+++ project/Landing.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CineMatch</title>
+</head>
+<body>
+
+ <h1>Cine</h1>
+    <h1>Match</h1>
+    
+</body>
+</html>
