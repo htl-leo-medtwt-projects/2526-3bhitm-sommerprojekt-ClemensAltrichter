0d7357af0314312285900c09c5d2945ce0a397d5
Clemens Altrichter <c.altrichter@students.htl-leonding.ac.at>
2026-03-24 16:19:39 +0100

    Refactor Landing page structure and add initial CSS file

--- project/Landing.php
+++ project/Landing.php
@@ -9,11 +9,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CineMatch</title>
+    <link rel="stylesheet" href="/style/global.css">
+    <link rel="stylesheet" href="./style/landing.css">
 </head>
 <body>
 
- <h1>Cine</h1>
-    <h1>Match</h1>
+    <div id="headerBox">
+        <h1>Cine</h1>
+        <h1>Match</h1>
+    </div>
+
+ 
     
 </body>
 </html>
